2e5e3c56c436e8571fa0157dcb0d7f590feff04f
wonda-tea-coffee <lagrange.resolvent@gmail.com>
2024-06-16 20:16:07 +0900

    fix patch condition

--- .github/scripts/bump.sh
+++ .github/scripts/bump.sh
@@ -10,11 +10,11 @@ major="${tokens[0]}"; minor="${tokens[1]}"; patch="${tokens[2]}";
 case "$1" in
   major) major="$((major + 1))"; minor=0; patch=0 ;;
   minor) minor="$((minor + 1))"; patch=0 ;;
-  major) patch="$((patch + 1))" ;;
+  patch) patch="$((patch + 1))" ;;
 esac
 
 # GitHubへフルバージョンタグとメジャーバージョンタグをプッシュ
-git tag "v${major}.${minor}.${patch}"
-git tag --force "v${major}" >/dev/null 2>&1
-git push --force --tags >/dev/null 2>&1
+# git tag "v${major}.${minor}.${patch}"
+# git tag --force "v${major}" >/dev/null 2>&1
+# git push --force --tags >/dev/null 2>&1
 echo "v${major}.${minor}.${patch}"
